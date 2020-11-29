574dc66376753821e8ee1361c7412aa0aed2469c
Adri <adrien.boitelle@gmail.com>
2020-11-29 18:05:32 +0100

    add default value for settings

---
+++ db/sql/zz_insert.sql
@@ -0,0 +1,11 @@
+use photodb;
+
+INSERT INTO setting (label)
+VALUES
+    ("mail","Not provided"),
+    ("birthday","Not provided"),
+    ("gender","Not provided"),
+    ("location","Not provided"),
+    ("bio",""),
+    ("victories",""),
+    ("points","");

2db267d3237655e8670e75b5897e2fb9b610ce28
rob <robiniscoding@gmail.com>
2020-11-30 18:25:15 +0100

    Now we can build war

--- back/src/main/java/routes/Posts.java
+++ back/src/main/java/routes/Posts.java
@@ -1,11 +1,18 @@
 package routes;
 
+import dao.PostDao;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
 @Path("posts/")
 public class Posts {
     @Inject PostDao postDao;
     
     @POST
     public Response getAll() {
-
+        return Response.ok().build();
     }
 }

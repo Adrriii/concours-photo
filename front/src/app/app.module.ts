2f979f974a731e206a7d36f3301da788a9c26651
Silverazz <alexandre.legendre@etu.u-bordeaux.fr>
2020-12-13 19:33:23 +0100

    merged from every repo

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { HeaderComponent } from './header/header.component';
-import {AppRoutingModule} from './app-routing.module';
 import {ReactiveFormsModule} from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';

8dc312361aabdf3a8936c80f048d7ca0238c12c4
rob <robiniscoding@gmail.com>
2020-12-13 15:44:59 +0100

    Correct imgur url

--- back/src/main/java/routes/Hello.java
+++ back/src/main/java/routes/Hello.java
@@ -1,16 +1,18 @@
 package routes;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 
-import services.implementation.imgur.ImgurImageService;
+import services.AbstractImageService;
 
 @Path("helloworld/")
 public class Hello {
+    @Inject AbstractImageService imageService;
+
     @GET
     public String helloWorld() throws Exception {
-        new ImgurImageService().postImage("https://i.imgur.com/Xz0uxJ0.jpeg");
+        imageService.postImage("https://www.redwolf.in/image/catalog/stickers/bongo-cat-sticker.jpg");
         return "Hello world !";
     }
 }
--- back/src/main/java/services/implementation/imgur/ImgurImageService.java
+++ back/src/main/java/services/implementation/imgur/ImgurImageService.java
@@ -2,6 +2,7 @@ package services.implementation.imgur;
 
 import java.io.InputStream;
 
+import org.apache.http.HttpResponse;
 import services.AbstractImageService;
 
 public class ImgurImageService implements AbstractImageService {
@@ -9,15 +10,17 @@ public class ImgurImageService implements AbstractImageService {
     @Override
     public String postImage(InputStream imageData) throws Exception {
 
-        System.out.println(HttpImgur.postItem("upload", imageData).toString());
+        //System.out.println(HttpImgur.postItem("upload", imageData).toString());
 
         return "";
     }
 
     @Override
     public String postImage(String imageUrl) throws Exception {
+        System.out.println("Posting image with url " + imageUrl);
+        HttpResponse response = HttpImgur.makePost("image", imageUrl);
 
-        System.out.println(HttpImgur.postItem("upload", imageUrl).toString());
+        System.out.println("Status code is : " + response.getStatusLine().getStatusCode());
 
         return "";
     }

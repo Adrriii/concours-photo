5139ec38e6597181da81a6d313abe9d34aec0ceb
Adri <adrien.boitelle@gmail.com>
2020-12-30 11:47:53 +0100

    add (untested) reaction route

--- back/src/main/java/ApplicationBinder.java
+++ back/src/main/java/ApplicationBinder.java
@@ -1,9 +1,7 @@
 import dao.*;
 import dao.sql.*;
 import org.glassfish.jersey.internal.inject.AbstractBinder;
-import services.AbstractImageService;
-import services.AuthenticationService;
-import services.PostService;
+import services.*;
 import services.implementation.imgur.ImgurImageService;
 
 public class ApplicationBinder extends AbstractBinder {
@@ -15,8 +13,10 @@ public class ApplicationBinder extends AbstractBinder {
         bind(SqlSettingDao.class).to(SettingDao.class);
         bind(ImgurImageService.class).to(AbstractImageService.class);
         bind(SqlCommentDao.class).to(CommentDao.class);
+        bind(SqlReactionDao.class).to(ReactionDao.class);
 
         bindAsContract(AuthenticationService.class);
         bindAsContract(PostService.class);
+        bindAsContract(ReactionService.class);
     }
 }
--- back/src/main/java/routes/Posts.java
+++ back/src/main/java/routes/Posts.java
@@ -1,19 +1,28 @@
 package routes;
 
-import dao.PostDao;
 import model.Post;
+import model.User;
+import services.AuthenticationService;
 import services.PostService;
+import services.ReactionService;
 
+import java.util.Optional;
+
+import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
-
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("posts/")
+@PermitAll
 public class Posts {
     @Inject PostService postService;
+    @Inject ReactionService reactionService;
+    @Inject AuthenticationService authenticationService;
     
     @POST
     @RolesAllowed("user")
@@ -26,6 +35,45 @@ public class Posts {
     @Path("{id}")
     public Response getById(@PathParam("id") int id) {
         return Response.ok().build();
+    }
+
+    @PUT
+    @RolesAllowed("user")
+    @Path("{id}/react")
+    public Response changeReactToPost(@Context HttpServletRequest req, @PathParam("id") int id, String reaction) {
+        Optional<User> userOp = authenticationService.getCurrentUser(req);
+        if(userOp.isPresent()) {
+            if(reactionService.changeReactToPost(id, userOp.get().id, reaction)) {
+                return Response.ok().build();
+            } else {
+                return Response.status(400).entity("Could not react to this post").build();
+            }
+        } else {
+            return Response.status(403).entity("Must be logged in to react to a post").build();
+        }
+    }
+
+    @POST
+    @RolesAllowed("user")
+    @Path("{id}/react")
+    public Response addReactToPost(@Context HttpServletRequest req, @PathParam("id") int id, String reaction) {
+        return changeReactToPost(req, id, reaction);
+    }
+
+    @DELETE
+    @RolesAllowed("user")
+    @Path("{id}/react")
+    public Response cancelReactToPost(@Context HttpServletRequest req, @PathParam("id") int id) {
+        Optional<User> userOp = authenticationService.getCurrentUser(req);
+        if(userOp.isPresent()) {
+            if(reactionService.cancelReactToPost(id, userOp.get().id)) {
+                return Response.ok().build();
+            } else {
+                return Response.status(400).entity("Could not cancel the reaction to this post").build();
+            }
+        } else {
+            return Response.status(403).entity("Must be logged in to remove a reaction to a post").build();
+        }
     }
 
     

7a44bba3460dde4a469a52762b6eff3872c2a600
rob <robiniscoding@gmail.com>
2021-01-12 16:13:29 +0100

    [WIP] Adding post service

---
+++ front/src/app/models/Post.model.ts
@@ -0,0 +1,18 @@
+import {Reactions} from './Reactions.model';
+import {User} from './User.model';
+import {Label} from './Label.model';
+import {Theme} from './Theme.model';
+
+export class Post {
+    constructor(
+        public title: string,
+        public reacted: string,
+        public reactions: Array<Reactions>,
+        public author: User,
+        public label: Label,
+        public theme: Theme,
+        public photo: string,
+        public photoDelete: string
+    ) {
+    }
+}
---
+++ front/src/app/services/posts.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Post} from '../models/Post.model';
+import {environment} from '../../environments/environment.prod';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostsService {
+
+    constructor(
+        private httpClient: HttpClient
+    ) {
+    }
+
+    getById(id: number): Observable<Post> {
+        return this.httpClient.get<Post>(environment.apiBaseUrl + `posts/${id}`);
+    }
+}

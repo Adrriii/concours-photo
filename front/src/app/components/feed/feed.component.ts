13fc4b973008000d5017e1c072598a171b958b5d
Silverazz <alexandre.legendre@etu.u-bordeaux.fr>
2021-01-14 02:27:57 +0100

    create post maj + fix profile out of page + post testing + comment section testing + add new comment testing + next theme start

--- front/src/app/components/feed/feed.component.ts
+++ front/src/app/components/feed/feed.component.ts
@@ -25,6 +25,7 @@ export class FeedComponent implements OnInit {
       dialogConfig.disableClose = true;
       dialogConfig.autoFocus = true;
       dialogConfig.backdropClass = 'backdropBackground';
+      dialogConfig.width = '30%';
 
       const dialogRef = this.dialog.open(CreatePostComponent, dialogConfig);
 

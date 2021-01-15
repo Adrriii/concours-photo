3a243011fcdf686401b847900fb2738b8c559945
Jean-David AKATA <akata.jda@gmail.com>
2021-01-15 14:13:04 +0100

    bugged

--- back/src/main/java/routes/Users.java
+++ back/src/main/java/routes/Users.java
@@ -1,15 +1,17 @@
 package routes;
 
 import filters.JWTTokenNeeded;
-import services.UserService;
-
-import javax.annotation.security.PermitAll;
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import model.User;
+import services.UserService;
+
+
 
 @Path("user/")
 @PermitAll
@@ -33,5 +35,18 @@ public class Users {
         return userService.getUserFromRequestContext(ctx).map(
                 user -> Response.ok().entity(user).build()
         ).orElse(Response.status(500).entity("Internal error, can't find logged user").build());
+    }
+
+    @PUT
+    @Path("me")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response modifyMe(User user) {
+        try {
+            return userService.update(user)
+                    .map(userUpdated -> Response.ok(userUpdated).build())
+                    .orElse(Response.status(400).entity("Internal error, cannot update logged user").build());
+        } catch (Exception e) {
+            return Response.status(500).entity(e.getMessage()).build();
+        }
     }
 }

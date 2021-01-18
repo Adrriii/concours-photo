fa3ace36b69953ce33d7502d9bdb3e850236af54
Adri <adrien.boitelle@gmail.com>
2021-01-18 18:30:30 +0100

    fix sorting

--- back/src/main/java/routes/Feed.java
+++ back/src/main/java/routes/Feed.java
@@ -29,8 +29,8 @@ public class Feed {
     @Produces(MediaType.APPLICATION_JSON)
     public Response feed(
             @Context ContainerRequestContext ctx,
-            @DefaultValue("DESC") @QueryParam("sort") String sort,
-            @DefaultValue("score") @QueryParam("direction") String direction,
+            @DefaultValue("DESC") @QueryParam("direction") String direction,
+            @DefaultValue("score") @QueryParam("sort") String sort,
             @DefaultValue("") @QueryParam("labels") String labels,
             @DefaultValue("1") @QueryParam("page") Integer page,
             @DefaultValue("15") @QueryParam("nbPosts") Integer nbPosts,

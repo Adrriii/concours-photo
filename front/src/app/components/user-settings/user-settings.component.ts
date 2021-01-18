1e878b2569e7fc5fb3e5ff4022846bab2be92db4
Jean-David AKATA <akata.jda@gmail.com>
2021-01-18 16:42:16 +0100

    delete photo

--- front/src/app/components/user-settings/user-settings.component.ts
+++ front/src/app/components/user-settings/user-settings.component.ts
@@ -7,17 +7,20 @@ import { User } from 'src/app/models/User.model';
 import { UserService } from 'src/app/services/user.service';
 import { AuthService } from '../../services/auth.service';
 import { EditSettingsComponent } from './edit-settings/edit-settings.component';
+import { CommonModule } from '@angular/common';  
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-user-settings',
-  templateUrl: './user-settings.component.html',
-  styleUrls: ['./user-settings.component.css']
+    selector: 'app-user-settings',
+    templateUrl: './user-settings.component.html',
+    styleUrls: ['./user-settings.component.css']
 })
 export class UserSettingsComponent implements OnInit, OnDestroy {
 
     currentUser: User = null;
     currentUserSubscription: Subscription;
     file: File;
+    public photo_url: string = 'https://i.imgur.com/0pRFULS.png';
 
     constructor(
         private dialog: MatDialog,
@@ -42,45 +45,45 @@ export class UserSettingsComponent implements OnInit, OnDestroy {
     }
 
     openDialog(): void {
-      const dialogConfig = new MatDialogConfig();
+        const dialogConfig = new MatDialogConfig();
 
-      dialogConfig.disableClose = true;
-      dialogConfig.autoFocus = true;
-      dialogConfig.backdropClass = 'backdropBackground';
-      dialogConfig.width = '80%';
-      dialogConfig.maxHeight = '80vh';
+        dialogConfig.disableClose = true;
+        dialogConfig.autoFocus = true;
+        dialogConfig.backdropClass = 'backdropBackground';
+        dialogConfig.width = '80%';
+        dialogConfig.maxHeight = '80vh';
 
-      const dialogRef = this.dialog.open(EditSettingsComponent, dialogConfig);
+        const dialogRef = this.dialog.open(EditSettingsComponent, dialogConfig);
 
-      dialogRef.afterClosed().subscribe(
-          data => console.log('Dialog output:', data)
-      );
+        dialogRef.afterClosed().subscribe(
+            data => console.log('Dialog output:', data)
+        );
     }
 
     getUserName() : string {
-      if(this.authService.currentUser)
-          return this.authService.currentUser.username;
-      return "Undefined";
+        if(this.authService.currentUser)
+            return this.authService.currentUser.username;
+        return "Undefined";
     }
 
     getSetting(settingName: string): string{
-      return this.authService.currentUser.getSetting(settingName);
+        if(this.authService.currentUser)
+          return this.authService.currentUser.getSetting(settingName);
+        return null
     } 
 
-    onFileChanged(event){
-      this.file = event.target.files[0];
+    onFileSelect(event): void{
+        this.file = event.target.files[0];
+        this.upload();
     }
 
-    onUpload(){
+    upload(): void{
         const uploadPicture = new FormData();
         uploadPicture.append('file', this.file, this.file.name);
         this.userService.updateProfilePicture(uploadPicture)
             .subscribe(
                 (user) => {
                     console.log('picture successfully uploaded : '+ JSON.stringify(user));
-                    // console.log(user)
-                    // console.log(user);
-                    // this.currentUser = User.fromJson(user);
                 },
                 (error) => {
                     this.toastr.error(error.message);
@@ -89,6 +92,15 @@ export class UserSettingsComponent implements OnInit, OnDestroy {
     }
 
     getPhoto(): string {
-      return this.authService.currentUser.photo;
+        if(this.authService.currentUser)
+          return this.authService.currentUser.photo;
+        return null;
+    }
+
+    deletePhoto(): void {
+        if(this.authService.currentUser){
+          this.authService.currentUser.photo = null;
+          this.authService.currentUser.photoDelete = null;
+        }
     }
 }
